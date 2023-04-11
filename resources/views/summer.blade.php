07e28f7e05bffca32a0233d1239bce3458927a96
denliedesign <customdenlie@gmail.com>
2023-04-11 10:20:06 -0500

    summer gfx update

--- resources/views/summer.blade.php
+++ resources/views/summer.blade.php
@@ -9,12 +9,17 @@
     <div class="my-5 py-md-5">
         <div class="container my-md-5 py-md-5">
             <h2 class="text-center text-uppercase">&mdash; Five Star Summer &mdash;</h2>
+            <div class="d-flex justify-content-center my-3">
+                <div class="btn btn-primary btn-lg">
+                    <a class="text-white text-decoration-none" href="https://app.akadadance.com/customer/login?schoolId=AK235599J&c=1" target="_blank">Enroll Today!</a>
+                </div>
+            </div>
             <div class="row mb-4">
                 <div class="col-sm">
-                    <img src="/images/five-star-summer-1.png" alt="summer schedule" class="img-fluid shadow rounded">
+                    <img src="/images/summer-1-6.png" alt="summer schedule" class="img-fluid shadow rounded">
                 </div>
                 <div class="col-sm">
-                    <img src="/images/five-star-summer-2.png" alt="summer schedule" class="img-fluid shadow rounded">
+                    <img src="/images/summer-7-and-up.png" alt="summer schedule" class="img-fluid shadow rounded">
                 </div>
             </div>
             <div class="d-flex justify-content-center mb-5">
@@ -24,13 +29,16 @@
             </div>
 {{--            <h2 class="text-center text-uppercase">&mdash; Summer &mdash;</h2>--}}
             <h2 class="text-center text-uppercase">&mdash; Passport to Summer Magic Mini Sessions &mdash;</h2>
-{{--            <div class="d-flex justify-content-center my-4">--}}
-{{--                <div class="btn btn-primary btn-lg">--}}
-{{--                    <a class="text-white text-decoration-none" href="https://app.akadadance.com/customer/login?schoolId=AK235599J&c=1" target="_blank">Enroll Today!</a>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-            <div class="d-flex justify-content-center">
-                <img src="/images/passport-to-summer-magic-2.png" alt="passport to summer magic" class="img-fluid shadow rounded">
+            <div class="d-flex justify-content-center my-4">
+                <div class="btn btn-primary btn-lg">
+                    <a class="text-white text-decoration-none" href="https://app.akadadance.com/customer/login?schoolId=AK235599J&c=1" target="_blank">Enroll Today!</a>
+                </div>
+            </div>
+            <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-2">
+                <img src="/images/camp-1.png" alt="passport to summer magic" class="img-fluid my-3 rounded">
+                <img src="/images/camp-2.png" alt="passport to summer magic" class="img-fluid my-3 rounded">
+                <img src="/images/camp-3.png" alt="passport to summer magic" class="img-fluid my-3 rounded">
+                <img src="/images/camp-4.png" alt="passport to summer magic" class="img-fluid my-3 rounded">
             </div>
             <p class="mt-3">
                 Join us this summer as we grab our passports and head on a magical adventure! Each week, participants will dance, explore, and use their imaginations as we travel to some of our favorite Orlando destinations!!
