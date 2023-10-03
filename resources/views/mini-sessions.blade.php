890bee6e6f3d7b321263a74b2cbe602d00709499
denliedesign <customdenlie@gmail.com>
2023-10-03 10:04:30 -0500

    broadway babies updated

--- resources/views/mini-sessions.blade.php
+++ resources/views/mini-sessions.blade.php
@@ -8,6 +8,33 @@
             <h2 class="text-center text-uppercase">&mdash; Mini Sessions &mdash;</h2>
 
             <div class="d-flex justify-content-center">
+                <img src="/images/broadway-babies-2.png" alt="baby in broadway outfit" class="img-fluid my-3 rounded" style="max-width: 525px; width: 100%; height: auto;">
+            </div>
+            <h2 class="text-center text-uppercase">&mdash; Broadway Babies Mini Session &mdash;</h2>
+            <p class="mt-3 text-center">
+                Broadway Babies is a beginning dance class for little ones and a caregiver! Get ready to move and groove in this fun, energetic class!
+                <br><br>
+                Saturdays 10:00-10:45AM
+                <br>October 14 - November 11
+                <br>Ages 1-2
+                <br>Tuition: $105
+
+                <br>Attire: Dancers can wear a leotard and tights or comfortable attire. Ballet shoes, bare feet, socks, or sneakers are also approved.
+                <br><br>
+                During the last week of class, November 11, you are welcome to invite a guest to observe the class.
+            </p>
+            <div class="d-flex justify-content-center my-4">
+                <div class="btn btn-primary btn-lg">
+                    <a class="text-white text-decoration-none" href="https://app.akadadance.com/customer/login?schoolId=AK235599J&c=1" target="_blank">Enroll!</a>
+                </div>
+            </div>
+            <hr class="my-3">
+            <!-- -->
+            <!-- -->
+            <!-- -->
+            <!-- -->
+
+            <div class="d-flex justify-content-center">
                 <img src="/images/Bluey.png" alt="little mermaid" class="img-fluid my-3 rounded" style="max-width: 525px; width: 100%; height: auto;">
             </div>
             <h2 class="text-center text-uppercase">&mdash; Bluey Dance Mini Session &mdash;</h2>
@@ -49,32 +76,6 @@
                 Attire: Kids should wear comfortable clothing/athletic attire and sneakers.
                 <br><br>
                 Parents will enjoy a mini-performance on the last day of class - November 6th!
-            </p>
-            <div class="d-flex justify-content-center my-4">
-                <div class="btn btn-primary btn-lg">
-                    <a class="text-white text-decoration-none" href="https://app.akadadance.com/customer/login?schoolId=AK235599J&c=1" target="_blank">Enroll!</a>
-                </div>
-            </div>
-            <hr class="my-3">
-            <!-- -->
-            <!-- -->
-            <!-- -->
-            <!-- -->
-            <div class="d-flex justify-content-center">
-                <img src="/images/mini-broadway.png" alt="baby in broadway outfit" class="img-fluid my-3 rounded" style="max-width: 525px; width: 100%; height: auto;">
-            </div>
-            <h2 class="text-center text-uppercase">&mdash; Broadway Babies Mini Session &mdash;</h2>
-            <p class="mt-3 text-center">
-                Broadway Babies is a beginning dance class for little ones and a caregiver! Get ready to move and groove in this fun, energetic class!
-                <br><br>
-                Thursdays 6:00-6:45PM
-                <br>October 5 - November 9
-                <br>Ages 1-2
-                <br>Tuition: $120
-
-                <br>Attire: Dancers can wear a leotard and tights or comfortable attire. Ballet shoes, bare feet, socks, or sneakers are also approved.
-                <br><br>
-                During the last week of class, November 9, you are welcome to invite a guest to observe the class.
             </p>
             <div class="d-flex justify-content-center my-4">
                 <div class="btn btn-primary btn-lg">
