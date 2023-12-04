6e5d3cda12500cd0ab7b90f61e2054b89dc90365
denliedesign <customdenlie@gmail.com>
2023-12-04 11:12:42 -0600

    mini camps winter

--- resources/views/mini-sessions.blade.php
+++ resources/views/mini-sessions.blade.php
@@ -8,80 +8,148 @@
             <h2 class="text-center text-uppercase">&mdash; Mini Sessions &mdash;</h2>
 
             <div class="d-flex justify-content-center">
-                <img src="/images/broadway-babies-2.png" alt="baby in broadway outfit" class="img-fluid my-3 rounded" style="max-width: 525px; width: 100%; height: auto;">
+                <img src="/images/Trolls.png" alt="mini camp" class="img-fluid my-3 rounded" style="max-width: 525px; width: 100%; height: auto;">
+            </div>
+            <h2 class="text-center text-uppercase">&mdash; Trolls Dance Party &mdash;</h2>
+            <p class="mt-3 text-center">
+                Let's have a dance party! Dancers will enjoy this fun dance class inspired by the music from the latest Trolls movie! No experience is necessary.
+                <br><br>
+                January 9 - February 13
+                <br>Tuesdays 6:30-7:15PM
+                <br>Ages 3-5
+                <br><br>
+                January 11 - February 15
+                <br>Thursdays 6:45-7:30PM
+                <br>Ages 5-7
+                <br><br>
+                Tuition: $149
+                <br><br>
+                Attire: Dancers can wear a leotard and tights or comfortable clothing/athletic attire. Ballet shoes or socks may be worn.
+                <br><br>
+                Parents may observe the last week of class!
+            </p>
+            <div class="d-flex justify-content-center my-4">
+                <div class="btn btn-primary btn-lg">
+                    <a class="text-white text-decoration-none" href="https://app.akadadance.com/customer/login?schoolId=AK235599J&c=1" target="_blank">Enroll!</a>
+                </div>
+            </div>
+            <hr class="my-3">
+            <!-- -->
+            <!-- -->
+            <!-- -->
+            <!-- -->
+
+            <div class="d-flex justify-content-center">
+                <img src="/images/Squishmallows.png" alt="mini camp" class="img-fluid my-3 rounded" style="max-width: 525px; width: 100%; height: auto;">
+            </div>
+            <h2 class="text-center text-uppercase">&mdash; Squishmallow Jam &mdash;</h2>
+            <p class="mt-3 text-center">
+                Bring your own Squishmallow or squishy stuffed animal to class each week as we jam to popular kids tunes!!
+                <br><br>
+                February 20 - March 26
+                <br>Tuesdays 6:30-7:15PM
+                <br>Ages 3-5
+                <br><br>
+                February 22 - March 28
+                <br>Thursdays 6:45-7:30PM
+                <br>Ages 5-7
+                <br><br>
+                Tuition: $149
+                <br><br>
+                Attire: Dancers can wear a leotard and tights or comfortable clothing/athletic attire. Ballet shoes or socks may be worn.
+                <br><br>
+                Parents may observe the last week of class!
+            </p>
+            <div class="d-flex justify-content-center my-4">
+                <div class="btn btn-primary btn-lg">
+                    <a class="text-white text-decoration-none" href="https://app.akadadance.com/customer/login?schoolId=AK235599J&c=1" target="_blank">Enroll!</a>
+                </div>
+            </div>
+            <hr class="my-3">
+            <!-- -->
+            <!-- -->
+            <!-- -->
+            <!-- -->
+
+            <div class="d-flex justify-content-center">
+                <img src="/images/broadway-babies-3.png" alt="baby in broadway outfit" class="img-fluid my-3 rounded" style="max-width: 525px; width: 100%; height: auto;">
             </div>
             <h2 class="text-center text-uppercase">&mdash; Broadway Babies Mini Session &mdash;</h2>
             <p class="mt-3 text-center">
                 Broadway Babies is a beginning dance class for little ones and a caregiver! Get ready to move and groove in this fun, energetic class!
                 <br><br>
-                Saturdays 10:00-10:45AM
-                <br>October 14 - November 11
-                <br>Ages 1-2
-                <br>Tuition: $105
-
-                <br>Attire: Dancers can wear a leotard and tights or comfortable attire. Ballet shoes, bare feet, socks, or sneakers are also approved.
+                <strong>Session 1:</strong>
+                <br>January 9 - February 13
+                <br>Thursdays 5:15-6:00PM or 6:00-6:45PM
                 <br><br>
-                During the last week of class, November 11, you are welcome to invite a guest to observe the class.
+                <strong>Session 2:</strong>
+                <br>February 22 - March 28
+                <br>Thursdays 5:15-6:00PM to 6:00-6:45PM
+
+                <br><br>Ages 1-2
+                <br><br>Tuition: $149
+                <br><br>Attire: Dancers can wear a leotard and tights or comfortable attire. Ballet shoes, bare feet, socks, or sneakers are also approved.
+                <br><br>Parents may observe the last week of class!
             </p>
             <div class="d-flex justify-content-center my-4">
                 <div class="btn btn-primary btn-lg">
                     <a class="text-white text-decoration-none" href="https://app.akadadance.com/customer/login?schoolId=AK235599J&c=1" target="_blank">Enroll!</a>
                 </div>
             </div>
-            <hr class="my-3">
+{{--            <hr class="my-3">--}}
             <!-- -->
             <!-- -->
             <!-- -->
             <!-- -->
 
-            <div class="d-flex justify-content-center">
-                <img src="/images/Bluey.png" alt="little mermaid" class="img-fluid my-3 rounded" style="max-width: 525px; width: 100%; height: auto;">
-            </div>
-            <h2 class="text-center text-uppercase">&mdash; Bluey Dance Mini Session &mdash;</h2>
-            <p class="mt-3 text-center">
-                3, 2, 1 Dance Mode!! Join us for our Bluey-themed Dance Mini Session for boys & girls!
-                <br>
-                <br>Tuesdays 6:30-7:15PM
-                <br>September 19 - October 24
-                <br>Ages 4-6
-                <br>Tuition: $120
-                <br>
-                Attire: Dancers can wear a leotard and tights or comfortable clothing/athletic attire. Ballet shoes or socks may be worn.
-                <br><br>
-                Parents will enjoy a mini-performance the last week of the class!
-            </p>
-            <div class="d-flex justify-content-center my-4">
-                <div class="btn btn-primary btn-lg">
-                    <a class="text-white text-decoration-none" href="https://app.akadadance.com/customer/login?schoolId=AK235599J&c=1" target="_blank">Enroll!</a>
-                </div>
-            </div>
-            <hr class="my-3">
+{{--            <div class="d-flex justify-content-center">--}}
+{{--                <img src="/images/Bluey.png" alt="little mermaid" class="img-fluid my-3 rounded" style="max-width: 525px; width: 100%; height: auto;">--}}
+{{--            </div>--}}
+{{--            <h2 class="text-center text-uppercase">&mdash; Bluey Dance Mini Session &mdash;</h2>--}}
+{{--            <p class="mt-3 text-center">--}}
+{{--                3, 2, 1 Dance Mode!! Join us for our Bluey-themed Dance Mini Session for boys & girls!--}}
+{{--                <br>--}}
+{{--                <br>Tuesdays 6:30-7:15PM--}}
+{{--                <br>September 19 - October 24--}}
+{{--                <br>Ages 4-6--}}
+{{--                <br>Tuition: $120--}}
+{{--                <br>--}}
+{{--                Attire: Dancers can wear a leotard and tights or comfortable clothing/athletic attire. Ballet shoes or socks may be worn.--}}
+{{--                <br><br>--}}
+{{--                Parents will enjoy a mini-performance the last week of the class!--}}
+{{--            </p>--}}
+{{--            <div class="d-flex justify-content-center my-4">--}}
+{{--                <div class="btn btn-primary btn-lg">--}}
+{{--                    <a class="text-white text-decoration-none" href="https://app.akadadance.com/customer/login?schoolId=AK235599J&c=1" target="_blank">Enroll!</a>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--            <hr class="my-3">--}}
             <!-- -->
             <!-- -->
             <!-- -->
             <!-- -->
 
-            <div class="d-flex justify-content-center">
-                <img src="/images/mini-mermaid.png" alt="little mermaid" class="img-fluid my-3 rounded" style="max-width: 525px; width: 100%; height: auto;">
-            </div>
-            <h2 class="text-center text-uppercase">&mdash; The Little Mermaid Musical Theater Mini Session &mdash;</h2>
-            <p class="mt-3 text-center">
-                Sing, dance, and act it out in our Little Mermaid Musical Theater Mini Session, inspired by the popular animated movie. No experience is necessary.
-                <br>
-                <br>Mondays 6:45-7:30PM
-                <br>October 2 - November 6
-                <br>Ages 5-8
-                <br>Tuition: $120
-                <br>
-                Attire: Kids should wear comfortable clothing/athletic attire and sneakers.
-                <br><br>
-                Parents will enjoy a mini-performance on the last day of class - November 6th!
-            </p>
-            <div class="d-flex justify-content-center my-4">
-                <div class="btn btn-primary btn-lg">
-                    <a class="text-white text-decoration-none" href="https://app.akadadance.com/customer/login?schoolId=AK235599J&c=1" target="_blank">Enroll!</a>
-                </div>
-            </div>
+{{--            <div class="d-flex justify-content-center">--}}
+{{--                <img src="/images/mini-mermaid.png" alt="little mermaid" class="img-fluid my-3 rounded" style="max-width: 525px; width: 100%; height: auto;">--}}
+{{--            </div>--}}
+{{--            <h2 class="text-center text-uppercase">&mdash; The Little Mermaid Musical Theater Mini Session &mdash;</h2>--}}
+{{--            <p class="mt-3 text-center">--}}
+{{--                Sing, dance, and act it out in our Little Mermaid Musical Theater Mini Session, inspired by the popular animated movie. No experience is necessary.--}}
+{{--                <br>--}}
+{{--                <br>Mondays 6:45-7:30PM--}}
+{{--                <br>October 2 - November 6--}}
+{{--                <br>Ages 5-8--}}
+{{--                <br>Tuition: $120--}}
+{{--                <br>--}}
+{{--                Attire: Kids should wear comfortable clothing/athletic attire and sneakers.--}}
+{{--                <br><br>--}}
+{{--                Parents will enjoy a mini-performance on the last day of class - November 6th!--}}
+{{--            </p>--}}
+{{--            <div class="d-flex justify-content-center my-4">--}}
+{{--                <div class="btn btn-primary btn-lg">--}}
+{{--                    <a class="text-white text-decoration-none" href="https://app.akadadance.com/customer/login?schoolId=AK235599J&c=1" target="_blank">Enroll!</a>--}}
+{{--                </div>--}}
+{{--            </div>--}}
         </div>
     </div>
 
