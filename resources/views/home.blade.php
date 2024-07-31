a20be10a7529459aa521a4a5ef5856045df58c17
denliedesign <customdenlie@gmail.com>
2024-07-31 12:13:20 -0500

    enrollment fair graphic

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -51,6 +51,10 @@
             </div>
         </div>
 
+        <div class="d-flex justify-content-center mt-4">
+            <img src="/images/enrollment-fair.png" alt="" class="img-fluid">
+        </div>
+
 {{--        <video controls poster="/images/edt-fun.png" style="width: 100%;" class="rounded shadow">--}}
 {{--            <source src="" type="video/mp4">--}}
 {{--        </video>--}}
